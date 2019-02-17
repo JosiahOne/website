f35a825bc4f8c9fe0a35c8c890eb9a9d41448105
Josiah Bruner <jsbruner@umich.edu>
2019-02-16 21:04:36 -0500

    Add files

---
+++ www/MozConfig.sh
@@ -0,0 +1,6 @@
+mk_add_options MOZ_OBJDIR=/Users/mozilla/Development/comm-central/obj-x86_64-apple-darwin13.1.0/
+ac_add_options --with-macos-sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
+ac_add_options --enable-debug
+ac_add_options --disable-optimize
+ac_add_options --enable-application=mail
+export MOZ_DEBUG_SYMBOLS=1
